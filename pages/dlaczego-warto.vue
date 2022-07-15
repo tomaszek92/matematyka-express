9bd881fd4602d56b284b385b415ed257a45431de
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2022-07-15 20:32:15 +0200

    layout fixes

--- pages/dlaczego-warto.vue
+++ pages/dlaczego-warto.vue
@@ -2,19 +2,20 @@
   <div class="wrapper grid grid-cols-1 gap-4">
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
       <div class="self-center justify-self-center">
-        <page-title>
+        <div class="title font-extrabold text-6xl xl:text-8xl mt-8 lg:mt-0 text-center">
           <span>Dlaczego<br>warto?</span>
-        </page-title>
+          <slot></slot>
+        </div>
       </div>
       <div class="self-center justify-self-center">
-        <img class="h-24 lg:h-36 xl:h-56" src="img/dlaczego-warto/main.png" alt="main"/>
+        <img class="h-36" src="img/dlaczego-warto/main.png" alt="main"/>
       </div>
-      <div class="self-center justify-self-center text-black text-center">
-        Jeśli szukasz kursu lub korepetycji, które skutecznie przygotują Cię do egzaminu ósmych klas, matury lub po
-        prostu kogoś, kto pomoże Ci w bieżącej nauce, to dobrze trafiłeś :)
+      <div class="self-center text-center text-xl">
+        Jeśli szukasz kursu lub korepetycji, które&nbsp;skutecznie przygotują Cię do&nbsp;egzaminu ósmych klas,
+        matury&nbsp;lub&nbsp;po prostu kogoś, kto&nbsp;pomoże Ci&nbsp;w&nbsp;bieżącej nauce, to&nbsp;dobrze trafiłeś :)
       </div>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4 lg:px-8">
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-6">
       <dlaczego-warto-reason img-src="img/dlaczego-warto/doswiadczenie.png">
         <span>
           Ponad <strong>6 lat<br>
@@ -77,5 +78,11 @@ export default {
   .wrapper {
     min-height: calc(100vh - 6.5rem);
   }
+}
+
+.title {
+  background: linear-gradient(90.08deg, #360C71 0.11%, #8444DF 69.58%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 </style>
