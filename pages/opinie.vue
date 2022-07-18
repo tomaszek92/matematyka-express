943d13a3be3f9bebeee09617ae64d04756f9e3b6
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2022-07-18 08:20:52 +0200

    opinie page

--- pages/opinie.vue
+++ pages/opinie.vue
@@ -4,29 +4,22 @@
     <div class="slideshow-container">
 
       <!-- Full-width images with number and caption text -->
-      <div class="mySlides fade">
-        <div class="numbertext">1 / 3</div>
+      <div class="slide fade">
         <img src="/img/opinie/1.png" style="width:100%">
-        <div class="text">Caption Text</div>
       </div>
 
-      <div class="mySlides fade">
-        <div class="numbertext">2 / 3</div>
+      <div class="slide fade">
         <img src="/img/opinie/2.png" style="width:100%">
-        <div class="text">Caption Two</div>
       </div>
 
-      <div class="mySlides fade">
-        <div class="numbertext">3 / 3</div>
+      <div class="slide fade">
         <img src="/img/opinie/3.png" style="width:100%">
-        <div class="text">Caption Three</div>
       </div>
 
       <!-- Next and previous buttons -->
       <a class="prev" @click="plusSlides(-1)">&#10094;</a>
       <a class="next" @click="plusSlides(1)">&#10095;</a>
     </div>
-    <br>
 
     <!-- The dots/circles -->
     <div style="text-align:center">
@@ -57,108 +50,84 @@ export default {
     },
     showSlides(n) {
       let i;
-      const slides = document.getElementsByClassName("mySlides");
+      const slides = document.getElementsByClassName("slide");
       const dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {this.slideIndex = 1}
-      if (n < 1) {this.slideIndex = slides.length}
+      if (n > slides.length) {
+        this.slideIndex = 1
+      }
+      if (n < 1) {
+        this.slideIndex = slides.length
+      }
       for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
       }
       for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
       }
-      slides[this.slideIndex-1].style.display = "block";
-      dots[this.slideIndex-1].className += " active";
+      slides[this.slideIndex - 1].style.display = "block";
+      dots[this.slideIndex - 1].className += " active";
     }
   }
 }
 </script>
 
 <style scoped>
-* {
-  box-sizing: border-box
-}
-
 /* Slideshow container */
 .slideshow-container {
-  max-width: 1000px;
+  width: 300px;
   position: relative;
   margin: auto;
 }
 
-/* Hide the images by default */
-.mySlides {
+.slide {
   display: none;
 }
 
-/* Next & previous buttons */
 .prev, .next {
   cursor: pointer;
   position: absolute;
   top: 50%;
   width: auto;
-  margin-top: -22px;
-  padding: 16px;
-  /*color: white;*/
+  margin-top: -1.5rem;
+  padding: -2rem;
   font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
+  font-size: 2rem;
+  transition: 0.3s;
   user-select: none;
-  color: red;
+  color: #360C71;
 }
 
-/* Position the "next button" to the right */
+.prev {
+  left: -1rem;
+}
+
 .next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
+  right: -1rem;
 }
 
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0, 0, 0, 0.8);
+.prev:hover,
+.next:hover {
+  color: #5B2AA0;
 }
 
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
 .dot {
   cursor: pointer;
-  height: 15px;
-  width: 15px;
+  height: 1rem;
+  width: 1rem;
   margin: 0 2px;
   background-color: #bbb;
   border-radius: 50%;
   display: inline-block;
-  transition: background-color 0.6s ease;
+  transition: background-color 0.3s;
 }
 
 .active, .dot:hover {
   background-color: #717171;
 }
 
-/* Fading animation */
 .fade {
   animation-name: fade;
-  animation-duration: 1.5s;
+  animation-duration: 0.3s;
 }
 
 @keyframes fade {
