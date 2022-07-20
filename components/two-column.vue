930e99aa205bc8cd50d90e6698c9d9ed538abf5b
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2022-07-20 18:11:58 +0200

    korepetycje page

---
+++ components/two-column.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <div class="grid grid-cols-1 gap-6 justify-items-center">
+      <div class="title text-center font-extrabold">
+        <slot name="title"/>
+      </div>
+      <div class="text-center">
+        <slot name="subtitle"/>
+      </div>
+      <div>
+        <img :src="imgSrc" :alt="imgSrc"/>
+      </div>
+    </div>
+    <div class="text-center">
+      <slot name="description"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MatematykaExpressTwoColumn",
+  props: {
+    imgSrc: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- pages/korepetycje.vue
+++ pages/korepetycje.vue
@@ -1,5 +1,73 @@
 <template>
-  <span>Korepetycje</span>
+  <div>
+    <two-column img-src="/img/korepetycje/korepetycje_index_1.png">
+      <template #title>
+        <span class="text-5xl xl:text-8xl">
+          Korepetycje
+        </span>
+      </template>
+      <template #subtitle>
+        Jeśli szukasz kogoś, kto skutecznie przygotuje Cię do&nbsp;egzaminu ósmych klas, matury
+        lub&nbsp;po&nbsp;prostu pomoże w&nbsp;bieżącej nauce, to&nbsp;dobrze trafiłeś. :)
+      </template>
+      <template #description>
+        Korepetycjami zajmuję się już <strong>od ponad sześciu lat</strong>. Przez te sześć lat pomagałam wielu osobom,
+        a&nbsp;nasza współpraca zawsze kończyła&nbsp;się sukcesem. Uczniowie wracają do mnie w&nbsp;kolejnym roku
+        szkolnym, a&nbsp;także zaczynam uczyć również ich rodzeństwo i&nbsp;znajomych.<br/>
+        <br/>
+        Na lekcjach ze mną <strong>nie musisz się stresować</strong>, że&nbsp;czegoś nie umiesz. <strong>Ja jestem tu po
+        to, aby&nbsp;wytłumaczyć Ci</strong> i&nbsp;powtórzyć dane zagadnienie tyle razy, ile będzie trzeba.<br/>
+        <br/>
+        Do nauki do matury mam opracowane materiały, które w&nbsp;szybki sposób pozwolą nam powtórzyć wszystkie
+        potrzebne zagadnienia. <strong>Wiem też jakie są tak zwane pewniaki</strong>, które na pewno na maturze się
+        pojawią. :)<br/>
+        <br/>
+        Zajęcia prowadzę zarówno w formie <strong>stacjonarnej</strong>, jak i&nbsp;<strong>online</strong>.
+      </template>
+    </two-column>
+
+    <div class="mb-8"></div>
+
+    <two-column img-src="/img/korepetycje/korepetycje_index_2.png">
+      <template #title>
+        <span class="text-3xl xl:text-5xl">
+          Jak wygląda współpraca ze&nbsp;mną?
+        </span>
+      </template>
+      <template #subtitle>
+        Zanim podejmiesz współpracę ze mną, warto abyś dowiedział się, czego wymagam od swoich uczniów i&nbsp;co sama
+        daje od siebie. :)
+      </template>
+      <template #description>
+        <div class="mb-1">
+          <strong>Od swoich uczniów wymagam:</strong>
+        </div>
+        <ul>
+          <li>
+            <strong>Chęci do pracy.</strong> Bez tego ani rusz. Daj od siebie chęci, a&nbsp;gwarantuję Ci, że ja podczas
+            naszej współpracy przekażę Ci całą moją wiedzę z&nbsp;danego tematu i&nbsp;w&nbsp;pełni zaangażuje się
+            w&nbsp;powierzone mi zadanie.
+          </li>
+          <li>
+            <strong>Prowadzenia notatek.</strong> Niby banał, ale bez notatek samodzielne rozwiązanie prac domowych jest
+            praktycznie niemożliwe!
+          </li>
+          <li>
+            <strong>Odrabiania prac domowych.</strong> To najważniejsza część naszej umowy. Godzinna lekcja ze mną to za
+            mało. Musisz sam robić zadania, bo tylko wtedy okaże się, czy naprawdę zrozumiałeś dany temat.
+          </li>
+        </ul>
+        <br/>
+        <span>
+          <strong>WAŻNE!</strong> Jeśli czegoś nie wiesz, napotkałeś na jakiś problem przy rozwiązywaniu zadania podczas
+          pracy domowej, nie musisz czeka do kolejnych zajęć. Możesz napisać mi SMS, wysłać mi wiadomość na Facebook'u
+          lub po prostu zadzwonić, a ja zawsze chętnie Ci pomogę. <br/>
+          <br/>
+          <strong>PAMIĘTAJ. TY i JA działamy w jednym teamie. :)</strong>
+        </span>
+      </template>
+    </two-column>
+  </div>
 </template>
 
 <script>
@@ -7,3 +75,11 @@ export default {
   name: "MatematykaExpressKorepetycjePage"
 }
 </script>
+
+<style scoped>
+li {
+  list-style-position: inside;
+  list-style-type: disclosure-closed;
+  padding: 0.25rem 0;
+}
+</style>
