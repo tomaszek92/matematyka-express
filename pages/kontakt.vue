c2f26d45917776ed112a5ab8b0fa30035195b6b0
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2022-07-23 09:47:05 +0200

    kontakt page

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,5 +1,34 @@
 <template>
-  <span>Kontakt</span>
+  <div>
+    <div class="title text-center text-4xl md:text-7xl font-extrabold mb-4">
+      Skontaktuj się
+    </div>
+    <div class="text-center text-lg font-medium mb-4">
+      JESTEM TU DLA CIEBIE
+    </div>
+    <div class="subtitle text-center text-xl font-extrabold mb-4">
+      Matematyka Express
+    </div>
+    <div class="text-center">
+      ul. Patriotów 303<br/>
+      04-767 Warszawa<br/>
+      Lokal nad Cafe Peron<br/>
+      piętro 2.<br/>
+      <a class="" href="tel:+4866206360">
+        <span>+48 662 063 460</span>
+      </a>
+      <br/>
+      <a class="" href="mailto:kontakt@matematyka-express.edu.pl">
+        <span>kontakt@matematyka-express.edu.pl</span>
+      </a>
+    </div>
+    <div class="mt-8">
+      <iframe
+        width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=pl&amp;q=patriot%C3%B3w%20303+(Nazwa%20mojej%20firmy)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+      </iframe>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +36,9 @@ export default {
   name: "MatematykaExpressKontaktPage"
 }
 </script>
+
+<style scoped>
+.subtitle {
+  color: #360C71;
+}
+</style>
