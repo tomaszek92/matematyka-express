455fad68689b43e4b9ef6b3775318cb85577f32b
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2022-07-24 12:35:21 +0200

    add sitemap.xml

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,9 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    '@nuxtjs/sitemap'
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
