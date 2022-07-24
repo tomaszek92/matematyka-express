59bc61be156ee9eb097c32d662aba47c81c8d0ff
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2022-07-24 15:08:53 +0200

    add 404 redirect to index

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,10 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  generate: {
+    fallback: true,
+  },
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
