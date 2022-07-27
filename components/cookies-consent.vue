31bfb20a54d058cd0441b8bba206ff46267a46aa
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2022-07-27 12:57:00 +0200

    add cookies consent

--- components/cookies-consent.vue
+++ components/cookies-consent.vue
@@ -1,27 +1,29 @@
 <template>
-  <div
-    v-if="!areCookiesAccepted"
-    class="cookie-consent fixed bottom-8 left-4 right-4 md:left-12 md:right-12 lg:left-auto flex rounded-2xl shadow-xl p-8 bg-white items-center"
-  >
-    <div class="text-sm">
-      Korzystając ze strony internetowej Matematyka Express, zgadzasz się na
-      <a
-        href="/statues/Polityka prywatności- Matematyka Express.pdf"
-        target="_blank"
-        class="underline"
-      >
-        politykę prywatności
-      </a>
+  <transition>
+    <div
+      v-if="!areCookiesAccepted"
+      class="cookie-consent fixed bottom-8 left-4 right-4 md:left-12 md:right-12 lg:left-auto flex rounded-2xl shadow-xl p-8 bg-white items-center"
+    >
+      <div class="text-sm">
+        Korzystając ze strony internetowej Matematyka Express, zgadzasz się na
+        <a
+          href="/statues/Polityka prywatności- Matematyka Express.pdf"
+          target="_blank"
+          class="underline"
+        >
+          politykę prywatności
+        </a>
+      </div>
+      <div>
+        <button
+          class="button text-xs text-white uppercase font-extrabold px-4 py-2 rounded-xl ml-4 md:whitespace-nowrap"
+          @click="onAccept"
+        >
+          zgadzam się
+        </button>
+      </div>
     </div>
-    <div>
-      <button
-        class="button text-xs text-white uppercase font-extrabold px-4 py-2 rounded-xl ml-4 md:whitespace-nowrap"
-        @click="onAccept"
-      >
-        zgadzam się
-      </button>
-    </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -50,3 +52,15 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.75s;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
