b1f4300cf21f19e6a68769ca43fb0a477c86e784
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2022-08-09 16:11:33 +0200

    change description

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
       {
         name: 'description',
         content:
-          'Korepetycje, kursy maturalne oraz ósmoklasisty z matematyki w Warszawie.',
+          'Jeśli szukasz kursu lub korepetycji, które skutecznie przygotują Cię do egzaminu ósmych klas, matury lub po prostu kogoś, kto pomoże Ci w bieżącej nauce, to dobrze trafiłeś.',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
