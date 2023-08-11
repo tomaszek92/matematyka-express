2e2ad0856da7aec2e56e7d8144af8596fddb2225
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2023-08-11 22:26:13 +0200

    upgrade packages

--- components/kursy/sections/matura-poziom-podstawowy.vue
+++ components/kursy/sections/matura-poziom-podstawowy.vue
@@ -1,25 +1,25 @@
 <template>
-  <span>todo</span>
-
-  <!--  <kursy-course-details-->
-  <!--    name="matura-poziom-podstawowy"-->
-  <!--    main-img-src="/img/kursy/matura_podstawowa_main.png"-->
-  <!--    :effects="effects"-->
-  <!--    :schedule="schedule"-->
-  <!--    :price="99"-->
-  <!--    :promo-price="79"-->
-  <!--    :duration-in-min="120"-->
-  <!--  >-->
-  <!--    <template #title>-->
-  <!--      <span>-->
-  <!--        Kurs MATURA POZIOM PODSTAWOWY przeznaczony jest dla osób,-->
-  <!--        które&nbsp;chcą przygotować się do egzaminu maturalnego. Potrzebują-->
-  <!--        odświeżenia wiedzy zdobytej w&nbsp;poprzednich latach i&nbsp;nadrobienia-->
-  <!--        ewentualnych braków.-->
-  <!--      </span>-->
-  <!--    </template>-->
-  <!--    <template #subtitle> Jedne zajęcia trwają 2 godziny. </template>-->
-  <!--  </kursy-course-details>-->
+  <kursy-course-details
+    name="matura-poziom-podstawowy"
+    main-img-src="/img/kursy/matura_podstawowa_main.png"
+    :effects="effects"
+    :schedule-stationary="schedule.stationary"
+    :schedule-online="schedule.online"
+    :single-lesson-duration-in-min="120"
+    :single-lesson-price="100"
+    :all-lessons-duration-in-hours="56"
+    :all-lessons-price="2800"
+  >
+    <template #title>
+      <span>
+        Kurs MATURA POZIOM PODSTAWOWY przeznaczony jest dla osób,
+        które&nbsp;chcą przygotować się do egzaminu maturalnego. Potrzebują
+        odświeżenia wiedzy zdobytej w&nbsp;poprzednich latach i&nbsp;nadrobienia
+        ewentualnych braków.
+      </span>
+    </template>
+    <template #subtitle> Jedne zajęcia trwają 2 godziny. </template>
+  </kursy-course-details>
 </template>
 
 <script>
--- components/kursy/sections/matura-poziom-rozszerzony.vue
+++ components/kursy/sections/matura-poziom-rozszerzony.vue
@@ -1,24 +1,27 @@
 <template>
-  <span>todo</span>
-  <!--  <kursy-course-details-->
-  <!--    name="matura-poziom-rozszerzony"-->
-  <!--    main-img-src="/img/kursy/matura_rozszerzona_main.png"-->
-  <!--    :effects="effects"-->
-  <!--    :schedule="schedule"-->
-  <!--    :price="149"-->
-  <!--    :promo-price="119"-->
-  <!--    :duration-in-min="180"-->
-  <!--  >-->
-  <!--    <template #title>-->
-  <!--      <span>-->
-  <!--        Kurs MATURA POZIOM ROZSZERZONY to intensywna powtórka materiału. Jest-->
-  <!--        dla osób, które&nbsp;chcą przygotować się do&nbsp;egzaminu maturalnego.-->
-  <!--        Potrzebują odświeżenia wiedzy zdobytej w&nbsp;poprzednich latach-->
-  <!--        i&nbsp;nadrobienia ewentualnych braków.-->
-  <!--      </span>-->
-  <!--    </template>-->
-  <!--    <template #subtitle> Jedne zajęcia trwają 3 godziny. </template>-->
-  <!--  </kursy-course-details>-->
+  <kursy-course-details
+    name="matura-poziom-rozszerzony"
+    main-img-src="/img/kursy/matura_rozszerzona_main.png"
+    :effects="effects"
+    :schedule-stationary="schedule.stationary"
+    :schedule-online="schedule.online"
+    :single-lesson-duration-in-min="90"
+    :single-lesson-price="75"
+    :all-lessons-duration-in-hours="56"
+    :all-lessons-price="4350"
+  >
+    <template #title>
+      <span>
+        Kurs MATURA POZIOM ROZSZERZONY to intensywna powtórka materiału. Jest
+        dla osób, które&nbsp;chcą przygotować się do&nbsp;egzaminu maturalnego.
+        Potrzebują odświeżenia wiedzy zdobytej w&nbsp;poprzednich latach
+        i&nbsp;nadrobienia ewentualnych braków.
+      </span>
+    </template>
+    <template #subtitle>
+      Zajęcia odbywają sie dwa razy w tygodniu. Jedno spotkanie trwa 1,5h.
+    </template>
+  </kursy-course-details>
 </template>
 
 <script>
